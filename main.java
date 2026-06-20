e07852ad8ff76372a71688cd8b20257303ed7ef5
Aaditya Passi <200138469+Aaditya-018@users.noreply.github.com>
2026-06-20 23:40:16 +0530

    Remove personal message from main.java

--- main.java
+++ main.java
@@ -2,7 +2,6 @@ package TicToe;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("Aaditya You are the Best ");
         System.out.println("My First Project in Java");
     }
 }
